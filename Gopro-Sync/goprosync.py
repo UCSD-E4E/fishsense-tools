502910bd780d8a18e7b5c829e88bf57337d5a08f
Vivaswat Suresh <vivaswat.suresh@gmail.com>
2023-06-02 12:32:39 -0700

    Removed unnecessary imports and created requirements.txt file with necessary libraries.

--- Gopro-Sync/goprosync.py
+++ Gopro-Sync/goprosync.py
@@ -3,17 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import stumpy
 import os
-import sys
 from moviepy.editor import VideoFileClip 
-import tempfile
 from pathlib import Path
+import argparse
+
 
 def gopro_sync(left, right, trimmed_left, trimmed_right):
-
-    left = Path(left)
-    right = Path(right)
-    trimmed_left = Path(trimmed_left)
-    trimmed_right = Path(trimmed_right)
 
     # Create a temporary directory to store the audio files
     
@@ -106,100 +101,57 @@ def gopro_sync(left, right, trimmed_left, trimmed_right):
     left_video = VideoFileClip(left.as_posix())
     left_video_trimmed = left_video.subclip(times_left[left_motif_index],times_left[-1])
     left_video_trimmed.write_videofile(trimmed_left.as_posix())
+    left_video.close()
+    left_video_trimmed.close()
 
     right_video = VideoFileClip(right.as_posix())
     right_video_trimmed = right_video.subclip(times_right[right_motif_index],times_right[-1])
     right_video_trimmed.write_videofile(trimmed_right.as_posix())
+    right_video.close()
+    right_video_trimmed.close()
 
 
-def argument_parser(args):
-
-    # make sure flags are used correctly
-    acceptable_args = ["--left", "--out", "--right"]
-    args_1 = args[1:3]
-    args_2 = args[3:5]
-    args_3 = args[5:7]
-
-    if args_1[0] not in acceptable_args:
-        print(f"{args_1[0]} is not an acceptable flag. Please use the flags --left, --right, --out")
-        exit()
-    elif args_2[0] not in acceptable_args:
-        print(f"{args_2[0]} is not an acceptable flag. Please use the flags --left, --right, --out")
-        exit()
-    elif args_3[0] not in acceptable_args:
-        print(f"{args_3[0]} is not an acceptable flag. Please use the flags --left, --right, --out")
-        exit()
-
-    if len(set([args_1[0], args_2[0], args_3[0]])) != 3:
-        print("Duplicate flags. Please try again")
-        exit()
-
-    # Getting all the paths organized into the appropriate variables
-    path_left, path_right, path_out = ("", "", "")
-
-    if (args_1[0] == "--left"):
-        path_left = args_1[1]
-        if (args_2[0] == "--right"):
-            path_right = args_2[1]
-            path_out = args_3[1]
-        else:
-            path_out = args_2[1]
-            path_right = args_3[1]
-
-    elif (args_1[0] == "--right"):
-        path_right = args_1[1]
-        if (args_2[0] == "--left"):
-            path_left = args_2[1]
-            path_out = args_3[1]
-        else:
-            path_out = args_2[1]
-            path_left = args_3[1]
+def main():
     
-    elif (args_1[0] == "--out"):
-        path_out = args_1[1]
-        if (args_2[0] == "--left"):
-            path_left = args_2[1]
-            path_right = args_3[1]
-        else:
-            path_right = args_2[1]
-            path_left = args_3[1]        
+    parser = argparse.ArgumentParser()
 
-    # Ensure paths exist
-    if not os.path.isfile(path_left):
-        print(f"{path_left} is not an acceptable path for flag --left, which should point to a file.")
+    parser.add_argument('--left', type=Path, required=True)
+    parser.add_argument('--right', type=Path, required=True)
+    parser.add_argument('--out', type=Path, required=True)
+
+    args = parser.parse_args()
+
+    # Make sure that all the paths are valid
+
+    if not args.left.is_file():
+        print("Left path is not a file.")
         exit()
-    elif not os.path.isfile(path_right):
-        print(f"{path_right} is not an acceptable path for flag --right, which should point to a file.")
+    
+    elif not args.right.is_file():
+        print("Right path is not file")
         exit()
-    elif not os.path.isdir(path_out):
-        print(f"{path_out} is not an acceptable path for flag --out, which should point to a directory.")
+    
+    elif not args.out.is_dir():
+        print("Out path is not a directory")
         exit()
+    
+    # Make sure that input files are mp4s
 
-    # Ensure that left and right files are mp4s
-    if (path_left[-4:] != ".mp4" and path_left[-4:] != ".MP4"):
-        print("File for left is not an mp4. Please try again.")
+    if (args.left.as_posix()[-4:] != ".mp4" and args.left.as_posix()[-4:] != ".MP4"):
+        print("Ensure that left file is an mp4")
         exit()
-    elif (path_right[-4:] != ".mp4" and path_right[-4:] != ".MP4"):
-        print("File for right is not an mp4. Please try again.")
+    
+    if (args.right.as_posix()[-4:] != ".mp4" and args.right.as_posix()[-4:] != ".MP4"):
+        print("Ensure that right file is an mp4")
         exit()
+    
+    left_trimmed = args.out.joinpath("left_trimmed.mp4")
+    right_trimmed = args.out.joinpath("right_trimmed.mp4")
 
-    return path_left, path_right, path_out
+    gopro_sync(left=args.left, right=args.right, trimmed_left=left_trimmed, trimmed_right=right_trimmed)
 
-# Check number of arguments is correct
-total_arguments = len(sys.argv)
-if (total_arguments != 7):
-
-    print("Incorrect number of arguments. Please ensure there are 6 arguments after the file name. For example: ")
-    print("python3 goprosync.py --left left.mp4 --right right.mp4 --out ./final/")
-    exit()
-
-# Ensure that all arguments are made correctly
-left_path, right_path, out_path = argument_parser(sys.argv)
-left_trimmed = Path(out_path, "left_trimmed.mp4")
-right_trimmed = Path(out_path, "right_trimmed.mp4")
-
-gopro_sync(left=left_path, right=right_path, trimmed_left=left_trimmed, trimmed_right=right_trimmed)
-
+if __name__ == '__main__':
+    main()
 
 
 
